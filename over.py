1ff70bd6f477450275c31b9055c2fe8d0c45c3c9
akshaya2248 <146075091+akshaya2248@users.noreply.github.com>
2023-10-06 16:00:31 +0530

    Add files via upload

---
+++ over.py
@@ -0,0 +1,12 @@
+a=int(input("enter the limitsnof the list"))
+i=0
+list=[]
+while i<a:
+       Z=int(input("enter the number"))
+       if Z>100:
+            list.append("over")
+       else:
+            list.append(Z)
+       i=i+1
+       print(list)
+                    
